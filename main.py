005bb5b0ce3b8f167f15d0fa2614ffdb375f3c9c
sad.wang <sad912@foxmail.com>
2022-09-05 16:47:33 +0800

    feat:add infer section

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from config import DefaultConfig as Config
 from torch.utils.data import DataLoader
 from model import LSTM
 from train import train
+from infer import infer
 
 if __name__ == "__main__":
     torch.use_deterministic_algorithms(True)
 
     flow_data = FlowDataset('train')
+    infer_data = FlowDataset('infer')
     train_data, eval_data, test_data = flow_data.split_data(flow_data, [8, 1, 1])
     train_data_loader = DataLoader(train_data, batch_size=Config.batch_size, shuffle=True, drop_last=True)
     eval_data_loader = DataLoader(eval_data, batch_size=Config.batch_size, shuffle=False, drop_last=True)
     test_data_loader = DataLoader(test_data, batch_size=Config.batch_size, shuffle=False, drop_last=True)
+    infer_data_loader = DataLoader(infer_data, batch_size=Config.batch_size, shuffle=False, drop_last=True)
     model = LSTM(
         Config.features_size,
         Config.hidden_size,
@@ -21,4 +24,4 @@ if __name__ == "__main__":
         Config.batch_size
     )
     train(Config, model, train_data_loader, eval_data_loader)
-
+    infer(Config, model, infer_data_loader, flow_data.mean, flow_data.standard)
